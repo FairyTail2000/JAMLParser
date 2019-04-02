5956caa79df6e9f030181a647d479df04fce3e19
FairyTail2000 <captn.rafaelh@gmail.com>
2019-04-02 17:32:25 +0200

    Added toString override to display the Content of the current JAML

--- jamlParser/main/JAMLParser.java
+++ jamlParser/main/JAMLParser.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -149,6 +148,14 @@ public class JAMLParser {
 		unparsed = new ArrayList<String>();
 		parsed = new ArrayList<String>();
 		DefinitionMap = new HashMap<String, String>();
+	}
+	
+	private String return_able = "";
+	@Override
+	public String toString() {
+		return_able += "Current Content:\n";
+		this.DefinitionMap.forEach((String Key, String Value) -> return_able += "Name: " + Key + ", Value: " + Value + "\n");
+		return return_able;
 	}
 	
 }
